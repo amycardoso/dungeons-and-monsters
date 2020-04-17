8a2c34a9794cd24acb2a90dea2c6e6d15f270aac
Amélia Alice <alice.cardoso10@gmail.com>
2020-04-17 11:44:39 -0300

    added movement to the hero

--- dmgame/src/components/Hero/index.tsx
+++ dmgame/src/components/Hero/index.tsx
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, { useState } from "react";
+import useEventListener from '@use-it/event-listener';
 import { TILE_SIZE, HEAD_OFFSET } from "../../settings/constants";
 
 import './index.css';
 
+const initialPosition = {
+    x: 1,
+    y: 2
+};
+
 const Hero = () => {
+    const [positionState, updatePositionState] = React.useState(initialPosition);
+    const [direction, updateDirectionState] = React.useState('RIGHT');
+
+    useEventListener("keydown", (event: any) => {
+        switch(event.key) {
+            case 'ArrowLeft':
+                updatePositionState({ x: positionState.x - 1, y: positionState.y });
+                updateDirectionState('LEFT');
+                break;
+            case 'ArrowRight':
+                updatePositionState({ x: positionState.x + 1, y: positionState.y });
+                updateDirectionState('RIGHT');
+                break;
+            case 'ArrowDown':
+                updatePositionState({ x: positionState.x, y: positionState.y - 1 });
+                break;
+            case 'ArrowUp':
+                updatePositionState({ x: positionState.x, y: positionState.y + 1 });
+                break;
+        }
+    });
+
     return (
-        <div 
+        <div
             style={{
                 position: 'absolute',
-                bottom: TILE_SIZE * 2,
-                left: TILE_SIZE * 1,
+                bottom: TILE_SIZE * positionState.y,
+                left: TILE_SIZE * positionState.x,
                 width: TILE_SIZE,
                 height: TILE_SIZE + HEAD_OFFSET,
-                backgroundImage:"url(./assets/HERO.png)",
+                backgroundImage: "url(./assets/HERO.png)",
                 backgroundRepeat: 'no-repeat',
                 backgroundPosition: `0 -${TILE_SIZE - HEAD_OFFSET}px`,
-                animation: 'hero-animation 1s steps(4) infinite'
+                animation: 'hero-animation 1s steps(4) infinite',
+                transform: `scaleX(${direction === 'RIGHT' ? 1 : -1})`
             }}
         />
     );
